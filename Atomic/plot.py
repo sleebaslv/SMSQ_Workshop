f5dde43970307510d807b67d19c0589fbe4234ca
Sleeba <sleebaslv@gmail.com>
2022-07-11 08:26:19 +1000

    made changes to atomic.lmp

--- Atomic/plot.py
+++ Atomic/plot.py
@@ -6,7 +6,7 @@ data2 = np.loadtxt('./allen.csv')
 
 plt.plot(data1[:,1], data1[:,2], color='r', label='Atomic system')
 plt.plot(data2[:,0], data2[:,1], linestyle='None', color='k', marker='o', markersize=4, label='Allen and Tildesley')
-plt.axhline(y=1, linestyle='--', color='r')
+plt.axhline(y=1.0, linestyle='--', color='r')
 plt.legend()
 plt.xlabel('$r/\\sigma$')
 plt.ylabel('$g(r)$')
